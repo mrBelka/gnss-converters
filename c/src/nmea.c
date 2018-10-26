56756e5847e61da6205505eb20fef25de87ae07c
Niilo Sirola <niilo.sirola@exafore.com>
2018-10-26 13:30:40 +0300

    fix some clang-tidy issues with nmea.c

--- c/src/nmea.c
+++ c/src/nmea.c
@@ -10,6 +10,8 @@
  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+#include "sbp_nmea_internal.h"
+
 #include <assert.h>
 #include <math.h>
 #include <stdarg.h>
@@ -23,7 +25,6 @@
 #include <swiftnav/gnss_time.h>
 #include <swiftnav/pvt_result.h>
 #include <swiftnav/signal.h>
-#include "sbp_nmea_internal.h"
 
 /** \addtogroup io
  * \{ */
@@ -88,10 +89,10 @@ typedef enum talker_id_e {
  * max_len = max possible length of the body of the message
  * (not including suffix)
  */
-#define NMEA_SENTENCE_START(max_len)            \
-  char sentence_buf[max_len + NMEA_SUFFIX_LEN]; \
-  char *sentence_bufp = sentence_buf;           \
-  char *const sentence_buf_end = sentence_buf + max_len;
+#define NMEA_SENTENCE_START(max_len)              \
+  char sentence_buf[(max_len) + NMEA_SUFFIX_LEN]; \
+  char *sentence_bufp = sentence_buf;             \
+  char *const sentence_buf_end = sentence_buf + (max_len);
 
 /** NMEA_SENTENCE_PRINTF: use like printf, can use multiple times
     within a sentence. */
@@ -851,7 +852,7 @@ bool check_nmea_rate(u32 rate, u32 gps_tow_ms, float soln_freq) {
    * message.
    * This way, we still send no_fix messages when receiver clock is drifting. */
   u32 soln_period_ms = (u32)(1.0 / soln_freq * 1e3);
-  u32 output_period_ms = (u32)soln_period_ms * rate;
+  u32 output_period_ms = soln_period_ms * rate;
   if (((gps_tow_ms) % output_period_ms) < (soln_period_ms / 2)) {
     return true;
   }
